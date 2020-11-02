47735b1c8021da5d3b5e234e2924fa01edefbbb1
teen <744124271@qq.com>
2020-11-02 22:32:58 +0800

    加油

---
+++ builtin.py
@@ -0,0 +1,98 @@
+1 abs
+求绝对值
+2 all
+3 any
+4 ascii
+5 bin
+6 bool
+7 breakpoint
+8 bytearray
+9 bytes
+10 callable
+11 chr
+将数值转化为对应字符
+12 classmethod
+13 compile
+14 complex
+将数值或字符串转化为复数
+15 copyright
+16 credits
+17 delattr
+18 dict
+字典函数
+19 dir
+20 divmod
+返回元祖，商与余
+21 enumerate
+22 eval
+评估函数
+23 exec
+类同 eval 更高级
+24 exit
+25 filter
+26 float
+将整数或字符串转化成浮点数
+27 format
+字符串格式化
+28 frozenset
+29 getattr
+30 globals
+31 hasattr
+32 hash
+33 help
+帮助函数
+34 hex
+十六进制
+35 id
+36 input
+从终端获取输入
+37 int
+将字符串或浮点数转换为整型
+38 isinstance
+39 issubclass
+40 iter
+41 len
+获取可迭代对象长度
+42 license
+43 list
+列表函数
+44 locals
+45 map
+46 max
+47 memoryview
+48 min
+49 next
+50 object
+51 oct
+十进制函数
+52 open
+文件函数
+53 ord
+将字符转化为对应的数值
+54 pow
+幂函数
+55 print
+在终端输出
+56 property
+57 quit
+58 range
+范围函数
+59 repr
+60 reversed
+61 round
+62 set
+集合函数
+63 setattr
+64 slice
+65 sorted
+排序函数
+66 staticmethod
+67 str
+字符串函数
+68 sum
+
+69 super
+70 tuple
+71 type
+72 vars
+73 zip
--- py.py
+++
@@ -1,2 +0,0 @@
-#Hello from computer 
-# from phone
