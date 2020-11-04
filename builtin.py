e6dfe3638600b07ef1329fb6c9ec6d02352758c0
teen <744124271@qq.com>
2020-11-04 13:26:48 +0800

    对Python内置函数的使用

--- builtin.py
+++ builtin.py
@@ -1,22 +1,47 @@
 1 abs
-求绝对值
+abs(-59)
+# 59
+
 2 all
 3 any
 4 ascii
+
 5 bin
+将数值二进制化
+
 6 bool
+
 7 breakpoint
+
 8 bytearray
+
 9 bytes
+
 10 callable
+
 11 chr
 将数值转化为对应字符
+
 12 classmethod
+
 13 compile
+
 14 complex
-将数值或字符串转化为复数
+complex(1, 2)
+(1 + 2j)
+complex(1)    # 数字
+(1 + 0j)
+complex("1")  # 当做字符串处理
+(1 + 0j)
+# 注意：在"+"号两边不能有空
+# 不能写成"1 + 2j"，否则会报错
+complex("1+2j")
+   (1 + 2j)
+
 15 copyright
+
 16 credits
+
 17 delattr
 18 dict
 字典函数
@@ -39,60 +64,116 @@
 30 globals
 31 hasattr
 32 hash
+
 33 help
-帮助函数
+help(str)
+help(sys)
+a = [1,2,3]
+help(a.append)
+
 34 hex
-十六进制
+hex(15)
+# 0x0F
+
 35 id
+
 36 input
-从终端获取输入
+input("input a number")
+
 37 int
-将字符串或浮点数转换为整型
+int("526")
+#526
+
+int(23.8)
+#23
+
 38 isinstance
 39 issubclass
 40 iter
+
 41 len
-获取可迭代对象长度
+a = "abcdedg"
+len(a)
+# 7
+
 42 license
+
 43 list
-列表函数
+#将可迭代对象转化成列表
+
 44 locals
+
 45 map
+
 46 max
+max(80, 100, 1000)
+# 1000
+
 47 memoryview
+
 48 min
+
 49 next
+
 50 object
+
 51 oct
 十进制函数
+
 52 open
 文件函数
+
 53 ord
 将字符转化为对应的数值
+
 54 pow
 幂函数
+
 55 print
 在终端输出
+
 56 property
+
 57 quit
+
 58 range
 范围函数
 59 repr
+
 60 reversed
+
 61 round
+round(58.28)
+round(53.99)
+#58
+#54
+
 62 set
-集合函数
+set("abcab")
+# {"a","b","c"}
+
 63 setattr
+
 64 slice
+
 65 sorted
 排序函数
+
 66 staticmethod
+
 67 str
 字符串函数
+
 68 sum
+求和
 
 69 super
+
 70 tuple
+
 71 type
+查看类型
+
 72 vars
+
 73 zip
